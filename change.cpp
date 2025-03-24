35656db88588655f6a71dba09d17083aa73f4cb3
abderry <derrybandoh100@gmail.com>
2025-03-24 14:39:16 +0000

    Version 2

--- change.cpp
+++ change.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 int main(){
-    cout<<"This is my first C++ codes, And I am loving it. Updated";
+    cout<<"This is my first C++ codes, And I am loving it. Updated"<<endl;
+    cout<< "That is how we do it here.";
     
     return 0;
 }
