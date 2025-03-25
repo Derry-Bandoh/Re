47ebc8789165853761fe1086040063642ed21feb
abderry <derrybandoh100@gmail.com>
2025-03-25 17:19:56 +0000

    Clone V

--- change.cpp
+++ change.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main(){
     cout<<"This is my first C++ codes";
     cout << "Upadated 12"<< endl;
-    cout << "The Version 13";
+    cout << "The Version 13"<< endl;
+    cout << "Clone Version";
     
     return 0;
 }
